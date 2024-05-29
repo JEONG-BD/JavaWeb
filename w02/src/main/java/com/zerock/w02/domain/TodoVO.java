eb9d35faa9c3836a1006a986514c3874e26efeae
JEONG-BD <dev.byeongdu@gmail.com>
2024-05-30 08:32:04 +0900

    VO

---
+++ w02/src/main/java/com/zerock/w02/domain/TodoVO.java
@@ -0,0 +1,22 @@
+package com.zerock.w02.domain;
+
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.time.LocalDate;
+
+@Setter
+@Getter
+@ToString
+public class TodoVO {
+
+    private Long tno;
+
+    private String titile;
+
+    private LocalDate dueDate;
+
+    private boolean finished;
+}
