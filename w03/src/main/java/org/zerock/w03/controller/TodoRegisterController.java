045fec874bb179da9e18d2d9b3e724ee2bf1e753
JEONG-BD <dev.byeongdu@gmail.com>
2024-06-03 14:32:40 +0900

    HttpSession

---
+++ w03/src/main/java/org/zerock/w03/controller/TodoRegisterController.java
@@ -0,0 +1,49 @@
+package com.zerock.w02.controller;
+
+
+import com.zerock.w02.dto.TodoDTO;
+import com.zerock.w02.service.TodoService;
+import lombok.extern.log4j.Log4j2;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+@WebServlet(name = "todoRegisterController", value="/todo/register")
+@Log4j2
+public class TodoRegisterController extends HttpServlet {
+
+    private TodoService todoService = TodoService.INSTANCE;
+    private final DateTimeFormatter DATEFORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+        log.info("/todo/register GET...");
+
+        req.getRequestDispatcher("/WEB-INF/todo/register.jsp").forward(req, res);
+    }
+
+    @Override
+    protected  void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+        log.info("/todo/register POST1...");
+        log.info(req);
+        TodoDTO todoDTO = TodoDTO.builder()
+                .title(req.getParameter("title"))
+                .dueDate(LocalDate.parse(req.getParameter("dueDate"),DATEFORMATTER))
+                .build();
+
+        log.info("/todo/register POST1...");
+        log.info(todoDTO);
+        try {
+            todoService.register(todoDTO);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        res.sendRedirect("/todo/list");
+    }
+}
