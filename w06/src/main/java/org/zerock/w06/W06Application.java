15bc87ad7b6732620744fd44206f4c5016d04356
JEONG-BD <dev.byeongdu@gmail.com>
2024-06-09 13:47:33 +0900

    Spring boot init

---
+++ w06/src/main/java/org/zerock/w06/W06Application.java
@@ -0,0 +1,13 @@
+package org.zerock.w06;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class W06Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(W06Application.class, args);
+    }
+
+}
---
+++ w06/src/test/java/org/zerock/w06/W06ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.zerock.w06;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class W06ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
