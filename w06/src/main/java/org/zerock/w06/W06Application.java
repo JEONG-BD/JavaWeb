8cf07ebddfe6d938201171056a6351fca5fc8de7
JEONG-BD <dev.byeongdu@gmail.com>
2024-06-10 08:06:44 +0900

    JPA

--- w06/src/main/java/org/zerock/w06/W06Application.java
+++ w06/src/main/java/org/zerock/w06/W06Application.java
@@ -2,8 +2,10 @@ package org.zerock.w06;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class W06Application {
 
     public static void main(String[] args) {
---
+++ w06/src/main/java/org/zerock/w06/domain/BaseEntity.java
@@ -0,0 +1,24 @@
+package org.zerock.w06.domain;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDate;
+
+@MappedSuperclass
+@EntityListeners(value = {AuditingEntityListener.class})
+public class BaseEntity {
+
+    @CreatedDate
+    @Column(name = "regdate", updatable = false)
+    private LocalDate regDate;
+
+    @LastModifiedDate
+    @Column(name = "moddate")
+    private LocalDate modDate;
+}
