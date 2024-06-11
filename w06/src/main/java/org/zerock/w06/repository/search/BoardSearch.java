66b363e567cce99c0a512bb3c5619d7e652a06d0
JEONG-BD <dev.byeongdu@gmail.com>
2024-06-11 15:10:06 +0900

    QueryDSL

--- w06/src/main/java/org/zerock/w06/repository/search/BoardSearch.java
+++ w06/src/main/java/org/zerock/w06/repository/search/BoardSearch.java
@@ -7,4 +7,6 @@ import org.zerock.w06.domain.Board;
 public interface BoardSearch {
 
     Page<Board> search1(Pageable pageable);
+
+    Page<Board> searchAll(String[] types, String keyword, Pageable pageable);
 }
