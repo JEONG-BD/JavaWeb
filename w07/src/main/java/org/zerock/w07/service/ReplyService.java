99137bba007b39d63f04725589c9dcf5c3c64c5f
JEONG-BD <dev.byeongdu@gmail.com>
2024-06-24 07:53:36 +0900

    Reply Service

--- w07/src/main/java/org/zerock/w07/service/ReplyService.java
+++ w07/src/main/java/org/zerock/w07/service/ReplyService.java
@@ -1,8 +1,18 @@
 package org.zerock.w07.service;
 
+import org.zerock.w07.dto.PageRequestDTO;
+import org.zerock.w07.dto.PageResponseDTO;
 import org.zerock.w07.dto.ReplyDTO;
 
 public interface ReplyService {
 
     Long register(ReplyDTO replyDTO);
+
+    ReplyDTO read(Long rno);
+
+    void modify(ReplyDTO replyDTO);
+
+    void remove(Long rno);
+
+    PageResponseDTO<ReplyDTO> getListOfBoard(Long bno, PageRequestDTO pageRequestDTO);
 }
