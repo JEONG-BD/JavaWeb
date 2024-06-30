f6ee5699766bd413054ae5ff7cb2fbd6e20ef350
JEONG-BD <dev.byeongdu@gmail.com>
2024-06-30 15:12:49 +0900

    Thumbnail

--- w08/src/main/java/org/zerock/w08/controller/UpDownController.java
+++ w08/src/main/java/org/zerock/w08/controller/UpDownController.java
@@ -5,13 +5,16 @@ import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Content;
 import jakarta.validation.Valid;
 import lombok.extern.log4j.Log4j2;
+import net.coobird.thumbnailator.Thumbnailator;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.zerock.w08.dto.upload.UploadFileDTO;
 
+import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.UUID;
@@ -40,6 +43,12 @@ public class UpDownController {
 
                 try {
                     multipartFile.transferTo(savePath);
+
+                    if(Files.probeContentType(savePath).startsWith("image")){
+                        File thumbFile = new File(uploadPath, "s_" + uuid + "_"+originalName);
+
+                        Thumbnailator.createThumbnail(savePath.toFile(), thumbFile, 200, 200);
+                    }
                 }catch (IOException e){
                     e.printStackTrace();
                 }
